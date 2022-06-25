38126aa6fbc375a6464a0f0a910b90d6b7a1708c
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-25 22:23:35 +0200

    add course Schema

---
+++ src/models/courseSchema.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import { AutoIncrementID } from '@typegoose/auto-increment';
+
+const schema = new mongoose.Schema({
+    _id: Number,
+    name: { type: String, required: true, unique: true, trim: true },
+    number_characters: { type: String, required: true, trim: true, },
+    teacher: { type: Number, ref: 'users' },
+}, { timestamps: true });
+
+schema.plugin(AutoIncrementID, [{ filed: '_id' }]);
+
+module.exports = mongoose.model('courses', schema)
