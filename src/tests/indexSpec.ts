7ef8fea4662fbee32003865eb8c517c789674da3
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-27 18:15:35 +0200

    start with testing

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -6,10 +6,28 @@ const request: SuperTest<Test> = supertest(app);
 
 describe('check Endpoint API', (): void => {
 
-    describe('check login Endpoint', (): void => {
-        it('POST /login', async (): Promise<void> => {
-            const response: Response = await request.post('/login');
-            expect(response.status).toBe(200);
+    describe('check user Endpoint', (): void => {
+        it('POST /user/login', async (): Promise<void> => {
+            const response: Response = await request.post('/user/login');
+            expect(response.status).toBe(500);
+        });
+        it('POST /user/register', async (): Promise<void> => {
+            const response: Response = await request.post('/user/register');
+            expect(response.status).toBe(500);
+        });
+        it('GET /user', async (): Promise<void> => {
+            const response: Response = await request.get('/user');
+            expect(response.status).toBe(500);
+        });
+        it('DELETE /user/logout', async (): Promise<void> => {
+            const response: Response = await request.delete('/user');
+            expect(response.status).toBe(500);
+        });
+        it('POST /user/logout', async (): Promise<void> => {
+            const response: Response = await (await request.post('/user/logout')).body({
+                "_id":0
+            });
+            expect(response.status).toBe(500);
         });
     });
 
@@ -19,8 +37,5 @@ describe('check Endpoint API', (): void => {
             expect(response.status).toBe(404);
         });
     });
-
-
-
 
 });
